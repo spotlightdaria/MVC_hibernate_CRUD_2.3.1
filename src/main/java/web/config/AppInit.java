d115e5a65a65a086c84748ad2fc314756e843f9b
spotlightdaria <spotlightdarya@gmail.com>
2022-07-07 23:31:47 +0300

    1

--- src/main/java/web/config/AppInit.java
+++ src/main/java/web/config/AppInit.java
@@ -3,7 +3,8 @@ package web.config;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import java.util.logging.Filter;
+import javax.servlet.Filter;
+
 
 public class AppInit extends AbstractAnnotationConfigDispatcherServletInitializer {
 
@@ -21,5 +22,11 @@ public class AppInit extends AbstractAnnotationConfigDispatcherServletInitialize
     protected String[] getServletMappings() {
         return new String[] {"/"};
     }
-
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        characterEncodingFilter.setForceEncoding(true);
+        return new Filter[] {characterEncodingFilter};
+    }
 }
